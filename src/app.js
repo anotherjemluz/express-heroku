c492d5497b5a64eb04c7ea4082f1839f8d4e2033
Jemima <contatojemima@gmail.com>
2020-01-07 11:54:48 -0300

    refatoração: movendo rotas de app.js para uma pasta
    1. criação da pasta routes/
    2. criação dos arquivos routes/users.js e config/routes.js
    
    organização de rotas agora obdece ao padrão módulo - função

--- src/app.js
+++ src/app.js
@@ -5,25 +5,13 @@ const consign = require('consign')
 // verbose false => omite a inicialização do consign
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
+  .then('./routes')
+  .then('./config/routes.js')
   .into(app)
 
-// criação de rota genérica
+// rota genérica
 app.get('/', (req, res) => {
   res.status(200).send()
-})
-
-// criação de rotas:
-// 1. get /users
-// 1. post /users
-app.get('/users', (req, res) => {
-  const users = [
-    { name: 'Jemima Luz', mail: 'jemima@mail.com' },
-  ]
-  res.status(200).json(users)
-})
-
-app.post('/users', (req, res) => {
-  res.status(201).json(req.body)
 })
 
 // exportando o objeto app
