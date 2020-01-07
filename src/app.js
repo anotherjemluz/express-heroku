c1f3c9f52cf148cc893fd40a2886803c9d33aa77
Jemima <contatojemima@gmail.com>
2020-01-07 11:30:13 -0300

    teste: inserção de usuário
    1. npm i  -S -E body-parser
    
    o body-parser irá servir para pegar o que será enviado no .send( ) e colocar no body da requisição

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 const app = require('express')()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 // criação de rota genérica
 app.get('/', (req, res) => {
   res.status(200).send()
 })
 
-// criação de rota genérica
+// criação de rotas:
+// 1. get /users
+// 1. post /users
 app.get('/users', (req, res) => {
   const users = [
     { name: 'Jemima Luz', mail: 'jemima@mail.com' },
   ]
   res.status(200).json(users)
+})
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body)
 })
 
 // exportando o objeto app
