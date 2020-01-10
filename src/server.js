20148b21ce647de2d261d9babb7e5115093de96c
Jemima <contatojemima@gmail.com>
2020-01-10 18:10:59 -0300

    a

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app')
 
 require('dotenv').config()
 
-app.set('port', process.env.PORT || 3000).
+const port = process.env.PORT || 3000
 
 // inicialização do servidor express
 app.listen(port , () => {
